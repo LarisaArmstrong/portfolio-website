b554d59da000f7dae35fb243d10b7f4ec10ef313
LarisaArmstrong <armstronglarisa@gmail.com>
2017-11-14 12:01:13 +0200

    video index intro
    js, query and video-js

--- js/application.js
+++ js/application.js
@@ -14,12 +14,19 @@ $(document).ready(function(){
 
     $("menu-container").click(function(){
         $(this);
-    });   
+    }); 
+    
+  
+    introVideo.play();
 
 
 ///////// END JQUERY /////////
 
 });
+
+
+
+///////// JAVASCRIPT //////////////
 
 
 // ------- menu bar ------- //
@@ -28,11 +35,27 @@ function menuBar(x) {
     x.classList.toggle("change");
 }
 
-// function showList (){
-//     var menu = document.getElementsByClassName("menu-container");
-//     var list = document.getElementsByClassName("dropList");
+// ----------- INTRO VIDEO JS --------//
+//-------------------------------------//
 
-//     if (menu.clicked == true){
-//         list.style.display = "block";
-//     };
-// };
+// videojs('intro', {
+//     controls: true,
+//     autoplay: true,
+//     preload: 'auto',
+//     loop: true,
+//   });
+  
+
+var introVideo = videojs('intro', {
+    autoplay: true,
+    preload: 'auto',
+    controls: false,
+    loop: true,
+});
+
+introVideo.on('canplaythrough', function(){
+    var body = document.getElementsByTagName('body')[0];
+    
+    body.classList.remove('is-loading');
+
+});
